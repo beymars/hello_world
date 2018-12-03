6c2ac8dfe292fab3b154da1de6fba4acd7994ceb
beymars <45576203+beymars@users.noreply.github.com>
2018-12-03 17:30:15 -0400

    Update and rename README.md to hola_mundo.php

---
+++ hola_mundo.php
@@ -0,0 +1,9 @@
+<? php
+  // En PHP, usamos echo para imprimir texto
+  echo  " hola mundo " ;
+  // Si quieres imprimir en la consola del navegador, usamos print_r
+  print_r ( " Hello World " );
+  // si quieres que los tipos de datos variables también usen var_dump
+  $ stringVar  =  ' hola mundo ' ;
+  var_dump ( $ stringVar );
+? >
